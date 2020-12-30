e7a95e0ef4209dcb0c89d823bf02ef20745ff405
michaeladze <michael.kutateladze@yandex.ru>
2020-12-30 14:05:53 +0300

    feat: [main] init

--- configs/config.js
+++
@@ -1,9 +0,0 @@
-import '../src/styles/index.scss';
-import { configure } from '@storybook/react';
-
-/** Вешаем дефолтный класс на body для применения цветовой схемы */
-function loadStories () {
-  document.body.className = 'theme-default';
-}
-
-configure(loadStories, module);
