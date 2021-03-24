93b6ad41b41a069d5bb65e69b830b9aec6abd0af
Пугачев Антон Александрович (4037116) <pugachev@dev.vtb.ru>
2021-03-24 18:17:51 +0300

    fix: [testrollup] fix

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+import url from 'rollup-plugin-url';
+import svgr from '@svgr/rollup';
+import react from 'react';
+import reactDom from 'react-dom';
+import { babel } from '@rollup/plugin-babel';
+import reactIs from 'react-is';
+
+export default {
+  input: 'src/index.ts',
+
+  output: [
+    {
+      dir: 'dist',
+      format: 'es'
+    }
+  ],
+  plugins: [
+    external(),
+    postcss({
+      modules: true,
+      extract: true,
+      minimize: true,
+      sourceMap: true
+    }),
+    url(),
+    svgr(),
+    resolve(),
+    typescript({
+      rollupCommonJSResolveHack: true,
+      clean: true,
+      exclude: ['src/**/*.stories.tsx', 'src/**/*.test.(tsx|ts)']
+    }),
+
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        'react': Object.keys(react),
+        'react-dom': Object.keys(reactDom),
+        'react-is': Object.keys(reactIs)
+      }
+    }),
+    babel({ babelHelpers: 'bundled' }),
+  ]
+};
