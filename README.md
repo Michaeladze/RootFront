9bfd02aed3fe733f5a9944f8b03b3ba8f730df73
michaeladze <michael.kutateladze@yandex.ru>
2021-04-16 09:15:57 +0300

    fix: [main] backdroploader + Tooltip size

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # RootFront
+
+Установить проект:
+```shell
+yarn root-front
+```
+
+Запустить проект:
+```shell
+yarn storybook
+```
--- src/components/atoms/BackdropLoader/BackdropLoader.tsx
+++ src/components/atoms/BackdropLoader/BackdropLoader.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Preloader from '../Preloader';
 import { Variant } from '../../../types';
+import { createPortal } from 'react-dom';
 
 export interface IBackdropLoaderProps {
   className?: string;
@@ -9,15 +10,26 @@ export interface IBackdropLoaderProps {
 
 const BackdropLoader: React.FC<IBackdropLoaderProps> = ({ className = '', variant }: IBackdropLoaderProps) => {
 
+  const [div] = useState<HTMLDivElement>(document.createElement('div'));
 
-  // -------------------------------------------------------------------------------------------------------------------
+  useEffect(() => {
+    document.body.appendChild(div);
+    document.body.style.overflowY = 'hidden';
 
+    return () => {
+      document.body.style.overflowY = 'auto';
+      document.body.removeChild(div);
+    };
+  });
 
-  return (
+  const loader = (
     <div className={`rf__backdrop-loader ${className}`}>
       <Preloader size='big' variant={variant}/>
     </div>
   );
+  // -------------------------------------------------------------------------------------------------------------------
+
+  return createPortal(loader, div);
 };
 
 export default BackdropLoader;
