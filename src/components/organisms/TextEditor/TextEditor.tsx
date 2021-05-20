360854bc356fc603e9b182b860e9fd3c1b6b93bf
michaeladze <michael.kutateladze@yandex.ru>
2021-05-20 12:58:30 +0300

    fix: [main] fix: [main] test quill

--- src/components/organisms/TextEditor/TextEditor.tsx
+++ src/components/organisms/TextEditor/TextEditor.tsx
@@ -3,7 +3,7 @@ import React, {
 } from 'react';
 import './TextEditor.scss';
 import ReactQuill from 'react-quill';
-import '../../../../styles/Quill.scss';
+import '../../../styles/Quill.scss';
 import Quill from 'quill';
 import { variants } from '../../../utils/helpers';
 import { Variant } from '../../../types';
