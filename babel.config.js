7d29c4d19758f16b9dd2d4e861eb3c9ad51d4e2f
michaeladze <michael.kutateladze@yandex.ru>
2021-07-02 09:43:06 +0300

    fix: [main] babel plugin for class proeprties

--- babel.config.js
+++ babel.config.js
@@ -54,6 +54,7 @@ module.exports = {
   "plugins": [
     "@babel/plugin-proposal-nullish-coalescing-operator",
     "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-class-properties",
     [
       hashPlugin,
       {
